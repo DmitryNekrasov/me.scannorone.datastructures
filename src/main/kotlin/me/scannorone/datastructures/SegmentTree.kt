f4403bea09477885e05f1c490ea9aace9f92cfa0
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-04-15 19:02:28 +0400

    Add SegmentTree

---
+++ src/main/kotlin/me/scannorone/datastructures/SegmentTree.kt
@@ -0,0 +1,81 @@
+package me.scannorone.datastructures
+
+class SegmentTree<T>(source: Collection<T>, private val operation: (T, T) -> T) {
+
+    private val size = source.size
+    private val tree = MutableList<T?>(size * 4) { null }
+
+    init {
+        if (source.isNotEmpty()) {
+            build(source, 1, 0, size - 1)
+        }
+    }
+
+    fun queryRange(queryStart: Int, queryEnd: Int): T? {
+        if (size == 0 || queryStart > queryEnd || queryStart < 0 || queryEnd >= size) {
+            return null
+        }
+        return queryRange(1, 0, size - 1, queryStart, queryEnd)
+    }
+
+    fun update(index: Int, newValue: T): Boolean {
+        if (index !in 0..<size) {
+            return false
+        }
+        update(1, 0, size - 1, index, newValue)
+        return true
+    }
+
+    private fun build(source: Collection<T>, nodeIndex: Int, start: Int, end: Int) {
+        if (start == end) {
+            tree[nodeIndex] = source.elementAt(start)
+        } else {
+            val mid = start + (end - start) / 2
+            val leftNodeIndex = nodeIndex * 2
+            val rightNodeIndex = nodeIndex * 2 + 1
+
+            build(source, leftNodeIndex, start, mid)
+            build(source, rightNodeIndex, mid + 1, end)
+
+            tree[nodeIndex] = operation(tree[leftNodeIndex]!!, tree[rightNodeIndex]!!)
+        }
+    }
+
+    private fun update(nodeIndex: Int, start: Int, end: Int, index: Int, newValue: T) {
+        if (start == end) {
+            tree[nodeIndex] = newValue
+        } else {
+            val mid = start + (end - start) / 2
+            val leftNodeIndex = nodeIndex * 2
+            val rightNodeIndex = nodeIndex * 2 + 1
+
+            if (index <= mid) {
+                update(leftNodeIndex, start, mid, index, newValue)
+            } else {
+                update(rightNodeIndex, mid + 1, end, index, newValue)
+            }
+
+            tree[nodeIndex] = operation(tree[leftNodeIndex]!!, tree[rightNodeIndex]!!)
+        }
+    }
+
+    private fun queryRange(nodeIndex: Int, start: Int, end: Int, queryStart: Int, queryEnd: Int): T? {
+        if (tree[nodeIndex] == null || queryEnd < start || queryStart > end) {
+            return null
+        }
+
+        if (queryStart <= start && end <= queryEnd) {
+            return tree[nodeIndex]
+        }
+
+        val mid = start + (end - start) / 2
+        val leftResult = queryRange(nodeIndex * 2, start, mid, queryStart, queryEnd)
+        val rightResult = queryRange(nodeIndex * 2 + 1, mid + 1, end, queryStart, queryEnd)
+
+        return when {
+            leftResult == null -> rightResult
+            rightResult == null -> leftResult
+            else -> operation(leftResult, rightResult)
+        }
+    }
+}
